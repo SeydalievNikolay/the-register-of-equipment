29c9efeabdc5636f9c5e757534d26dc69be60fc0
nk.seidaliev <nk.seidaliev@mish.design>
2024-10-07 15:23:16 +0700

    init

---
+++ demo/src/main/java/com/seydaliev/demo/TheRegisterOfEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.seydaliev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRegisterOfEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRegisterOfEquipmentApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/seydaliev/demo/TheRegisterOfEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seydaliev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRegisterOfEquipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
